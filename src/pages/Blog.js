391b41b43fa21ed13507892a2421b410d71ddb4b
Jweb60 <jy.babiarczyk@gmail.com>
2022-06-25 17:02:21 +0200

    Ajout Blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,17 +1,39 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
+import Article from "../components/Article";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 const Blog = () => {
+  const [blogData, setBlogData] = useState([]);
+  const [author, setAuthor] = useState("");
   const [content, setContent] = useState("");
   const [error, setError] = useState(false);
+
+  const getData = () => {
+    axios
+      .get("http://localhost:3004/articles")
+      .then((res) => setBlogData(res.data));
+  };
+  useEffect(() => getData(), []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
     if (content.length < 140) {
       setError(true);
     } else {
+      axios.post("http://localhost:3004/articles", {
+        author,
+        content,
+        date: Date.now(),
+      });
       setError(false);
+      setAuthor("");
+      setContent("");
+      getData();
     }
   };
 
@@ -22,16 +44,30 @@ const Blog = () => {
       <h1>Blog</h1>
 
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Nom" />
+        <input
+          type="text"
+          placeholder="Nom"
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+          value={author}
+        />
         <textarea
           style={{ border: error ? "2px solid red" : "1px solid #61dafb" }}
           placeholder="Message"
-          onChangeCapture={(e) => setContent(e.target.value)}
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
         ></textarea>
         {error && <p>Veuillez écrire un minimum de 140 caractères</p>}
         <input type="submit" value="Envoyer" />
       </form>
-      <ul></ul>
+      <ul>
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+      </ul>
     </div>
   );
 };
